d6fc38a0cbfc9bfc28a810f05140d9897cd533ed
plahhill <74708771+plahhill@users.noreply.github.com>
2020-11-19 21:50:31 +0700

    Add files via upload

---
+++ homeInteraction.js
@@ -0,0 +1,8 @@
+document.getElementById("link").addEventListener("click", function(){
+    this.style.color = "#eb2907";
+})
+
+document.getElementById("closebtn").addEventListener("click", function(){
+    mybox.style.display = "none";
+})
+
